06ece3f640901a2622be02ad869f0172570b7c82
abhisheksaini333 <abhishek_saini@live.com>
2021-09-24 01:31:52 +0530

    initial commit

---
+++ src/main/java/edureka/flowing/trip/saga/camunda/springboot/TripBookingSaga.java
@@ -0,0 +1,52 @@
+package edureka.flowing.trip.saga.camunda.springboot;
+
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import edureka.flowing.trip.saga.camunda.adapter.BookFlightAdapter;
+import edureka.flowing.trip.saga.camunda.adapter.BookHotelAdapter;
+import edureka.flowing.trip.saga.camunda.adapter.CancelCarAdapter;
+import edureka.flowing.trip.saga.camunda.adapter.CancelFlightAdapter;
+import edureka.flowing.trip.saga.camunda.adapter.CancelHotelAdapter;
+import edureka.flowing.trip.saga.camunda.adapter.ReserveCarAdapter;
+import edureka.flowing.trip.saga.camunda.springboot.builder.SagaBuilder;
+
+@Component
+//@Singleton
+public class TripBookingSaga {
+
+  @Autowired
+  private ProcessEngine camunda;
+
+  @PostConstruct
+  public void defineSaga() {
+    SagaBuilder saga = SagaBuilder.newSaga("trip") //
+        .activity("Reserve car", ReserveCarAdapter.class) //
+        .compensationActivity("Cancel car", CancelCarAdapter.class) //
+        .activity("Book hotel", BookHotelAdapter.class) //
+        .compensationActivity("Cancel hotel", CancelHotelAdapter.class) //
+        .activity("Book flight", BookFlightAdapter.class) //
+        .compensationActivity("Cancel flight", CancelFlightAdapter.class) //
+        .end() //
+        .triggerCompensationOnAnyError();
+
+    camunda.getRepositoryService().createDeployment() //
+        .addModelInstance("trip.bpmn", saga.getModel()) //
+        .deploy();
+
+//    File file = new File("result.bpmn");
+//    Bpmn.writeModelToFile(file, saga.getModel());
+  }
+  
+  public void bookTrip() {
+    HashMap<String, Object> someVariables = new HashMap<>();
+    // Could add some variables here - not used in simple demo
+    camunda.getRuntimeService().startProcessInstanceByKey("trip", someVariables);
+  }
+
+}
